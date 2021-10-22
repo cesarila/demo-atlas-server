0ff4dd4b21458dfe9ec7f1a0323ec557365eae4f
Luke <cesarila@rose-hulman.edu>
2021-10-22 17:27:29 -0400

    add happy path classes to tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,29 +5,31 @@ from app.models import Player
 from app.services import p2sr
 from app import api
 from .conftest import generate_test_player
-pytestmark = pytest.mark.usefixtures('mocked_newname')
 
 
-def test_get_valid_player_returns_200(db_session, client):
-    testPlayer = generate_test_player(db_session)
-    response = client.get(url_for('api.get_player_name', steam_id=testPlayer.steam_id))
-    assert response.status_code == 200
+@pytest.mark.usefixtures('mocked_newname')
+class TestHappyPaths:
 
-def test_get_valid_player_returns_correct_name(db_session, client):
-    testPlayer = generate_test_player(db_session)
-    response = client.get(url_for('api.get_player_name', steam_id=testPlayer.steam_id))
-    response_json = json.loads(response.data.decode().strip())
-    assert response_json == testPlayer.to_json()
+    def test_get_valid_player_returns_200(self, db_session, client):
+        testPlayer = generate_test_player(db_session)
+        response = client.get(url_for('api.get_player_name', steam_id=testPlayer.steam_id))
+        assert response.status_code == 200
 
-def test_get_expired_player_returns_200(db_session, client, force_update_condition):
-    testPlayer = generate_test_player(db_session)
-    response = client.get(url_for('api.get_player_name', steam_id=testPlayer.steam_id))
-    assert response.status_code == 200
+    def test_get_valid_player_returns_correct_name(self, db_session, client):
+        testPlayer = generate_test_player(db_session)
+        response = client.get(url_for('api.get_player_name', steam_id=testPlayer.steam_id))
+        response_json = json.loads(response.data.decode().strip())
+        assert response_json == testPlayer.to_json()
 
-def test_get_expired_player_returns_fresh_name(db_session, client, force_update_condition):
-    testPlayer = generate_test_player(db_session)
-    original_json = testPlayer.to_json()
-    response = client.get(url_for('api.get_player_name', steam_id=testPlayer.steam_id))
-    response_json = json.loads(response.data.decode().strip())
-    assert original_json != response_json
-    assert response_json == testPlayer.to_json()
+    def test_get_expired_player_returns_200(self, db_session, client, force_update_condition):
+        testPlayer = generate_test_player(db_session)
+        response = client.get(url_for('api.get_player_name', steam_id=testPlayer.steam_id))
+        assert response.status_code == 200
+
+    def test_get_expired_player_returns_fresh_name(self, db_session, client, force_update_condition):
+        testPlayer = generate_test_player(db_session)
+        original_json = testPlayer.to_json()
+        response = client.get(url_for('api.get_player_name', steam_id=testPlayer.steam_id))
+        response_json = json.loads(response.data.decode().strip())
+        assert original_json != response_json
+        assert response_json == testPlayer.to_json()
--- tests/test_player_controller.py
+++ tests/test_player_controller.py
@@ -2,29 +2,30 @@ import pytest
 from .conftest import generate_test_player
 from app.models import Player
 from app.controllers import player_controller
-pytestmark = pytest.mark.usefixtures('mocked_newname')
 
 
-def test_create_player(db_session):
-    assert Player.query.count() == 0
-    testPlayer = generate_test_player()
-    player_controller.create_player(steam_id=testPlayer.steam_id, display_name=testPlayer.display_name)
-    assert Player.query.count() == 1
-    dbPlayer = Player.query.filter_by(steam_id=testPlayer.steam_id).first()
-    assert dbPlayer.steam_id == testPlayer.steam_id
-    assert dbPlayer.display_name == testPlayer.display_name
-    assert dbPlayer.last_updated is not None
+@pytest.mark.usefixtures('mocked_newname')
+class TestHappyPath:
+    def test_create_player(self, db_session):
+        assert Player.query.count() == 0
+        testPlayer = generate_test_player()
+        player_controller.create_player(steam_id=testPlayer.steam_id, display_name=testPlayer.display_name)
+        assert Player.query.count() == 1
+        dbPlayer = Player.query.filter_by(steam_id=testPlayer.steam_id).first()
+        assert dbPlayer.steam_id == testPlayer.steam_id
+        assert dbPlayer.display_name == testPlayer.display_name
+        assert dbPlayer.last_updated is not None
 
 
-def test_update_player(db_session):
-    testPlayer = generate_test_player(db_session)
-    player_controller.update_player(testPlayer.steam_id)
-    assert testPlayer.display_name == 'verycoolupdatedname'
+    def test_update_player(self, db_session):
+        testPlayer = generate_test_player(db_session)
+        player_controller.update_player(testPlayer.steam_id)
+        assert testPlayer.display_name == 'verycoolupdatedname'
 
 
-def test_get_fresh_player_updates_after_threshold(db_session, client, force_update_condition):
-    testPlayer = generate_test_player(db_session)
-    player_controller.get_fresh_player(testPlayer.steam_id)
+    def test_get_fresh_player_updates_after_threshold(self, db_session, client, force_update_condition):
+        testPlayer = generate_test_player(db_session)
+        player_controller.get_fresh_player(testPlayer.steam_id)
 
-    dbPlayer = Player.query.filter_by(steam_id=testPlayer.steam_id).first()
-    assert dbPlayer.display_name == 'verycoolupdatedname'
+        dbPlayer = Player.query.filter_by(steam_id=testPlayer.steam_id).first()
+        assert dbPlayer.display_name == 'verycoolupdatedname'
